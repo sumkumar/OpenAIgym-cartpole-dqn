5dae4595776cd807a17800a8731f59b1340f1540
Sumanth Kumar <sumanth.education@gmail.com>
2022-08-15 03:46:12 +0530

    code cleanup

--- cartpole-example.py
+++ cartpole-example.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import DQN
+
 show = False
 vis_window = 100    # Visualization window
 
@@ -22,13 +24,13 @@ for i in range(exp_replay_size):
     if show:
         env.render()
     done = False
-    while (done != True):
+    while done != True:
         A = agent.get_action(obs, env.action_space.n, epsilon=1)
         obs_next, reward, done, _ = env.step(A.item())
         agent.collect_experience([obs, A.item(), reward, obs_next])
         obs = obs_next
         index += 1
-        if (index > exp_replay_size):
+        if index > exp_replay_size:
             break
 
 # Main training loop
@@ -39,7 +41,7 @@ epsilon = 1
 
 for i in tqdm(range(episodes)):
     obs, done, losses, ep_len, rew = env.reset(), False, 0, 0, 0
-    while (done != True):
+    while done != True:
         ep_len += 1
         A = agent.get_action(obs, env.action_space.n, epsilon)
         obs_next, reward, done, _ = env.step(A.item())
@@ -53,7 +55,7 @@ for i in tqdm(range(episodes)):
         rew += reward
         index += 1
 
-        if (index > 128):
+        if index > 128:
             index = 0
             for j in range(4):
                 loss = agent.train(batch_size=128)
@@ -61,8 +63,7 @@ for i in tqdm(range(episodes)):
     if epsilon > 0.05:
         epsilon -= (1 / 5000)
 
-    losses_list.append(losses / ep_len), reward_list.append(rew), episode_len_list.append(ep_len), epsilon_list.append(
-        epsilon)
+    losses_list.append(losses / ep_len), reward_list.append(rew), episode_len_list.append(ep_len), epsilon_list.append(epsilon)
 
 df = pd.DataFrame(list(zip(losses_list, reward_list, episode_len_list, epsilon_list)),
                columns =['Loss', 'Reward', 'Ep_len', 'Epsilon'])
